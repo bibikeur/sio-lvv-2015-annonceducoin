b2b82b646e1e87ec1bb76c1fb2c52e2380e379d3
michaudlouis0@gmail.com <michaudlouis0@gmail.com@642ce75b-d882-4ae6-4ffe-3d85136cb71c>
2014-12-08 10:43:11 +0000

    tret

--- AnnonceDuCoin/src/pckmetier/Departement.java
+++ AnnonceDuCoin/src/pckmetier/Departement.java
@@ -2,5 +2,24 @@ package pckmetier;
 
 public class Departement
 {
+	private String nomDep;
 
+	public Departement(String nomDep)
+	{
+		//super();
+		this.nomDep = nomDep;
+	}
+
+	public String getNomDep()
+	{
+		return nomDep;
+	}
+
+	public void setNomDep(String nomDep)
+	{
+		this.nomDep = nomDep;
+	}
+	
+	
+	
 }
