64a727ad4dce8227ce1b3d26f0b013e876fa26ff
simon.garnier2@gmail.com <simon.garnier2@gmail.com@642ce75b-d882-4ae6-4ffe-3d85136cb71c>
2014-12-09 15:27:15 +0000

    Création attributs/ accesseurs des classes

--- AnnonceDuCoin/src/pckmetier/Categorie.java
+++ AnnonceDuCoin/src/pckmetier/Categorie.java
@@ -2,5 +2,18 @@ package pckmetier;
 
 public class Categorie
 {
+	private String nomSousCateg;
+
+	public String getNomSousCateg()
+	{
+		return nomSousCateg;
+	}
+
+	public void setNomSousCateg(String nomSousCateg)
+	{
+		this.nomSousCateg = nomSousCateg;
+	}
+	
+	
 	
 }
--- AnnonceDuCoin/src/pckmetier/SousCategorie.java
+++ AnnonceDuCoin/src/pckmetier/SousCategorie.java
@@ -2,5 +2,17 @@ package pckmetier;
 
 public class SousCategorie
 {
+	private String nomCateg;
 
+	public String getNomCateg()
+	{
+		return nomCateg;
+	}
+
+	public void setNomCateg(String nomCateg)
+	{
+		this.nomCateg = nomCateg;
+	}
+	
+	
 }
