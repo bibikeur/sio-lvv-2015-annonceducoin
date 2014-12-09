a6cb876516b4b42d4b9affa0ab2b2b3e5549ab3b
simon.garnier2 <simon.garnier2@gmail.com>
2014-12-09 15:27:15 +0000

    Création attributs/ accesseurs des classes

--- AnnonceDuCoin/src/pckmetier/Photo.java
+++ AnnonceDuCoin/src/pckmetier/Photo.java
@@ -2,5 +2,17 @@ package pckmetier;
 
 public class Photo
 {
+	private String lienPhoto;
 
+	public String getLienPhoto()
+	{
+		return lienPhoto;
+	}
+
+	public void setLienPhoto(String lienPhoto)
+	{
+		this.lienPhoto = lienPhoto;
+	}
+	
+	
 }
